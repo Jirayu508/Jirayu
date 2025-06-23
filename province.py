49b68340f4c8c4d7d36ab797753a29657784e127
Jirayu508 <pooley38585@gmail.com>
2025-06-23 21:49:36 +0700

    Add files via upload

---
+++ province.py
@@ -0,0 +1,7 @@
+province =["กรุงเทพมหานคร","กาฬสินธุ์","จันทบุรี","ฉะเชิงเทรา","ชลบุรี","พะเยา","ชุมพร","ชัยนาท","ขอนเเก่น","ชัยภูทิ","ยโสธร","นครพนม","นครราชสีมา","เชียงใหม่"]
+province[0] ="Bangkok"
+province[4] ="Paet Riw"
+province[5] ="Chon buri"
+province[7] ="Chum porn"
+province[12] ="Korat"
+print(province)
