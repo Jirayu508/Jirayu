8efe2a1f155add7d49afe2fc38c0ea45bf6c1793
Jirayu508 <pooley38585@gmail.com>
2025-07-07 14:33:24 +0700

    Add files via upload

---
+++ การทำงานเเบบวนซ้ำ.py
@@ -0,0 +1,18 @@
+for i in range(1,101):
+    print(i)
+print("ทำงาน100ลูป i=100")
+for i in range(7,77,7):
+    print(i)
+print("ทำงาน9ลูป i=70")
+for i in range(20,1,-2):
+    print(i)
+print("ทำงาน8ลูป i=2")
+for i in range(2,18,3):
+    print(i)
+print("ทำงาน5ลูป i=17")
+for i in range(55,0,-11):
+    print(i)
+print("ทำงาน4ลูป i=11")
+for i in range(1,0):
+    print(i)
+print("ทำงาน0ลูป iไม่มีค่า")
