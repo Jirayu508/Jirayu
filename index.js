47b7ef1854d60f2b2c98fb456b67f1e1473cc803
Jirayu508 <pooley38585@gmail.com>
2026-02-24 21:10:51 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let n1 = 100;
+let n2 = 2.5;
+let s1 = "A";
+let s2 = "B";
+let s3 = "C5";
+
+console.log(n1);
+console.log(n2);
+console.log(s1);
+console.log(s2);
+console.log(s3);
+
+console.log(n1, n2);
+console.log(s1, s2);
+console.log(s3);
+
+console.log(n1 + "" + n2, s1 + s2, s3); 
+
+console.log(n1 + "Hello");
+console.log(n2 + "Hello");
+console.log("Hello" + s1);
+
+console.log(n1 + "Hello" + n2 + "Hello" + s1);
